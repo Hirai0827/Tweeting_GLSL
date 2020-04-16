a712a1d060c22f5a53b184cb019ee01cd461bd36
hirai <ryu1999827@gmail.com>
2020-04-16 16:31:06 +0900

    fix error log

--- src/GLSLErrorLog.js
+++ src/GLSLErrorLog.js
@@ -7,10 +7,18 @@ export class GLSLErrorLog extends Component {
 
     render() {
         let hasError = this.props.errorLog;
+        let text = "Compile Succeeded!";
+        if(hasError){
+            text = this.props.errorLog.split("\n")[0];
+            let sp = text.split(':');
+            sp[2] = (parseInt(sp[2])-8).toString();
+            text = sp.join(':');
+        }
         return (
             <div>
-                <div style={{lineHeight:"32px",width:"100%",textAlign:"left",backgroundColor:"black",color:(hasError?"pink":"lightgreen"),borderRadius:"4px"}}>
-                    {!hasError?"Compile Succeeded!":this.props.errorLog.split("\n")[0]}
+                <div style={{lineHeight:"32px",width:"100%",textAlign:"left",backgroundColor:"black",color:(hasError?"pink":"lightgreen"),borderRadius:"4px",overflow:"hidden",whiteSpace:"nowrap"}}>
+                    {text}
+
                 </div>
             </div>
         );
